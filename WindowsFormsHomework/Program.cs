ce5d656c9b4c7199ae1f6dcc26099340f2cda801
MinMiss5678 <83458914+MinMiss5678@users.noreply.github.com>
2021-11-08 10:01:50 +0800

    Update Program.cs

--- WindowsFormsHomework/Program.cs
+++ WindowsFormsHomework/Program.cs
@@ -13,7 +13,7 @@ namespace WindowsFormsHomework
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Loan());
+            Application.Run(new Student_StructForm());
         }
     }
 }
